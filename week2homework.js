d57cb9074ac9509565fa47c737f2892dfacaf30f
Joe <jspector906@gmail.com>
2020-04-18 10:18:23 -0500

    did some homework

---
+++ week2homework.js
@@ -0,0 +1,14 @@
+
+let pizzaPlace = "World's Finest Pizza";
+let numberOfToppings = 30;
+
+console.log(typeof "World's Finest Pizza");
+console.log(typeof 30);
+
+`"This Pizza," he said, "is the finest in the world!"`
+
+if (numberOfToppings < 10) {
+console.log(``"Quality, not quantity."`);
+} else (numberOfToppings > 10) {
+console.log(`"A whole lot of pizza."`);
+}
